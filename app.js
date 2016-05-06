1b46197e9ab8b2a2b9e83c126d2462ed94a07f56
lee <jiyangli@163.com>
2016-05-06 23:11:45 +0800

    修改swiper

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require("express");
 var path = require('path');
 var app = express();
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, 'public/swiper')));
 app.listen(3000);
 console.log("服务器已经启动!");
